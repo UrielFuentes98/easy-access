0ac17c7c0199a327e961d4e95cb11ac47e7eb0d5
Uriel Fuentes <urielfuentesc@gmail.com>
2022-02-23 22:33:42 -0600

    Adding close button to transfer access.

--- web/src/Pages/GetTransfer/index.tsx
+++ web/src/Pages/GetTransfer/index.tsx
@@ -1,5 +1,5 @@
-import { Box, Link, Text } from "@chakra-ui/layout";
-import { useAppSelector } from "app/hooks";
+import { Box, Text } from "@chakra-ui/layout";
+import { useAppDispatch, useAppSelector } from "app/hooks";
 import {
   selectTranAccessId,
   selectTransferId,
@@ -10,12 +10,28 @@ import {
   GET_FilesNames,
   GET_FILES_NAMES,
 } from "app/utils/api";
-import { DownloadIcon } from "@chakra-ui/icons";
+import { CloseIcon, DownloadIcon } from "@chakra-ui/icons";
+import { SITE_PATHS } from "app/routes";
+import { Link } from "react-router-dom";
+import {
+  setAnswer,
+  setSecretQuestion,
+  setTranAccessId,
+  setTransferId,
+} from "Pages/Landing/PhraseInput/transferAccessSlice";
 
 function GetTransfer() {
+  const dispatch = useAppDispatch();
   const accessId = useAppSelector(selectTranAccessId);
   const transferId = useAppSelector(selectTransferId);
   const [filesNames, setFilesNames] = useState([] as string[]);
+
+  function ResetTransInfo() {
+    dispatch(setTransferId(0));
+    dispatch(setSecretQuestion(""));
+    dispatch(setAnswer(""));
+    dispatch(setTranAccessId(""));
+  }
 
   useEffect(() => {
     async function getFilesNames() {
@@ -26,6 +42,7 @@ function GetTransfer() {
       }
     }
     getFilesNames();
+    return () => ResetTransInfo();
   }, []);
 
   function getDownloadUrl(fileName: string) {
@@ -34,20 +51,26 @@ function GetTransfer() {
 
   return (
     <Box
-      w={["2xs", null, "sm"]}
+      w={["3xs", null, "sm"]}
       py={6}
       px={[8, null, 20]}
       rounded={[20, null, 30]}
       bg="white"
     >
-      <Text
-        fontSize={["xl", null, "3xl"]}
-        fontWeight="bold"
-        pb={2}
-        textAlign="center"
-      >
-        Transfer
-      </Text>
+      <Box display="flex" alignItems="center">
+        <Text
+          fontSize={["xl", null, "3xl"]}
+          fontWeight="bold"
+          pb={2}
+          textAlign="center"
+          ml="auto"
+        >
+          Transfer
+        </Text>
+        <Link to={SITE_PATHS.LANDING} style={{ marginLeft: "auto" }}>
+          <CloseIcon color="red.600" boxSize={{ base: 3, lg: 5 }} />
+        </Link>
+      </Box>
       {filesNames.map((fileName, key) => (
         <Box
           display="flex"
@@ -56,13 +79,13 @@ function GetTransfer() {
           key={key}
         >
           <Text fontSize={{ base: "md", lg: "xl" }}>{fileName}</Text>
-          <Link href={getDownloadUrl(fileName)}>
+          <a href={getDownloadUrl(fileName)}>
             <DownloadIcon
               color="blue.600"
               boxSize={5}
               _hover={{ cursor: "pointer" }}
             />
-          </Link>
+          </a>
         </Box>
       ))}
     </Box>
